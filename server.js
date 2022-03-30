3ac44ff37a619c5ea146db5568badd4901fac41a
Mark J. Healy <gmarkghealy@gmail.com>
2022-03-30 17:47:34 -0400

    Query for Create candidates

--- server.js
+++ server.js
@@ -20,9 +20,36 @@ const db = mysql.createConnection(
   console.log("Connected to the election database")
 );
 
-db.query("SELECT * FROM candidates", (err, rows) => {
-  console.log(rows);
-});
+// db.query("SELECT * FROM candidates", (err, rows) => {
+//console.log(rows);
+//});
+
+// Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE ID = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE  id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 //Default response for any other request (NOT FOUND)
 //This route will override all others—so make sure that this is the last one.
